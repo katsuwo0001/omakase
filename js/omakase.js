8a001f65f4c225107334b9967866b3f026499701
katsuwo0001 <katsuwo.com@gmail.com>
2014-10-25 14:37:32 +0900

    Air詳細トグル表示
    AIRサマリ行のクリック時に、詳細エリアをToogleで制御するよう変更

--- js/omakase.js
+++ js/omakase.js
@@ -2,12 +2,9 @@ $(document).ready(function() {
 	console.log("ready start");
 
 	// 旅程リスト clickイベント
-	$('#tbl-itin-list tbody').on('click', 'tr', function(event) {
+	$('#tbl-itin-list tbody').on('click', '#air-summary', function(event) {
 		console.log($(this));
-		// 詳細入力領域チェック
-		// if(hasDetailArea($(this))){
-			crateAirDetail($(this));
-		// }
+		$(this).next("#air-detail").toggle();
 	});
 });
 // 更新ボタン
@@ -24,22 +21,22 @@ function hasDetailArea(target){
 }
 
 // AIR詳細Element生成
-function crateAirDetail(target){
-	// 行
-	var row = $('<tr>');
-	// 列
-	var col = $('<td>',{colspan:3}).appendTo(row);
-	// AIR詳細
-	// var air_detail = $('<div>',{id:'air-detail-panel'}).appendTo(col);
-	// var title = $('<div>',{text:'フライト詳細'}).appendTo(col);
-	var title = $('<div>',{text:'フライト詳細'}).appendTo(col);
-	var detail = $('<div>',{style:'margin:10px 10px 10px 10px;padding-right:10px;'}).appendTo(col);
-	$('<label>',{text:'予約番号'}).appendTo(detail);
-	$('<input>',{'type':'text'}).appendTo(detail);
+// function crateAirDetail(target){
+// 	// 行
+// 	var row = $('<tr>');
+// 	// 列
+// 	var col = $('<td>',{colspan:3}).appendTo(row);
+// 	// AIR詳細
+// 	// var air_detail = $('<div>',{id:'air-detail-panel'}).appendTo(col);
+// 	// var title = $('<div>',{text:'フライト詳細'}).appendTo(col);
+// 	var title = $('<div>',{text:'フライト詳細'}).appendTo(col);
+// 	var detail = $('<div>',{style:'margin:10px 10px 10px 10px;padding-right:10px;'}).appendTo(col);
+// 	$('<label>',{text:'予約番号'}).appendTo(detail);
+// 	$('<input>',{'type':'text'}).appendTo(detail);
 
-	// 更新ボタン
-	var buttons = $('<div>',{style:'margin:10px 10px 10px 10px;text-align:right;'}).appendTo(col);
-	$('<button>',{text:'更新',type:'button','class':'btn btn-primary btn-sm',id:'modify'}).appendTo(buttons);
+// 	// 更新ボタン
+// 	var buttons = $('<div>',{style:'margin:10px 10px 10px 10px;text-align:right;'}).appendTo(col);
+// 	$('<button>',{text:'更新',type:'button','class':'btn btn-primary btn-sm',id:'modify'}).appendTo(buttons);
 
-	row.insertAfter(target);
-}
+// 	row.insertAfter(target);
+// }
