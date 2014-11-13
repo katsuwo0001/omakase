6cf0d5c5c3dc2d86ab0a9c3cccc25fa1a46f8a77
k-hayama <k-hayama@itcjapan.com>
2014-11-13 20:11:14 +0900

    最新のモックをコミット

--- js/omakase.js
+++ js/omakase.js
@@ -40,3 +40,97 @@ function hasDetailArea(target){
 
 // 	row.insertAfter(target);
 // }
+
+
+//詳細表示チェックボックスイベント
+$('#detail-display').click(function () {
+
+	//詳細表示を閉じたり開いたり
+  	itineraryDetailDisplay();
+
+});
+
+//air表示チェックボックスイベント
+$('#air-display').click(function () {
+
+ 	if($('#air-display').prop('checked')) {
+	  	$('.itinerary-air').show();
+	  	itineraryDetailDisplay();
+
+ 	} else {
+ 		$('.itinerary-air').hide();
+ 		itineraryDetailDisplay();
+
+ 	}
+	
+});
+
+//trns表示チェックボックスイベント
+$('#trns-display').click(function () {
+
+ 	if($('#trns-display').prop('checked')) {
+	  	$('.itinerary-trns').show();
+	  	itineraryDetailDisplay();
+
+ 	} else {
+ 		$('.itinerary-trns').hide();
+ 		itineraryDetailDisplay();
+
+ 	}
+	
+});
+
+// 詳細表示処理
+function itineraryDetailDisplay() {
+
+	if($('#detail-display').prop('checked')) {
+		// 詳細表示の場合
+		if($('#air-display').prop('checked')) {
+			$('.itinerary-air-detail').show();
+		} else {
+			$('.itinerary-air-detail').hide();
+		}
+
+		if($('#trns-display').prop('checked')) {
+			$('.itinerary-trns-detail').show();
+		} else {
+			$('.itinerary-trns-detail').hide();
+		}
+
+	} else {
+		// 詳細非表示の場合
+		$('.itinerary-air-detail').hide();
+		$('.itinerary-trns-detail').hide();
+	}
+
+}
+
+// 登録部表示処理
+$('#entry-display').click(function (){
+	if($('#entry-display').text() == "▼表示") {
+		$('#entry-tab-cotent').show();
+		$('#entry-display').text("▲非表示");
+	
+	} else {
+		$('#entry-tab-cotent').hide();
+		$('#entry-display').text("▼表示");
+	}
+
+});
+
+$('.entry-tab').click(function (){
+	$('#entry-tab-cotent').show();
+	$('#entry-display').text("▲非表示");
+});
+
+// カレンダー表示(JquryUI)
+$(function() {
+  // 2日本語を有効化
+  $.datepicker.setDefaults($.datepicker.regional['ja']);
+  // 3日付選択ボックスを生成
+  $('.calendar').datepicker({ dateFormat: 'yy/mm/dd' });
+});
+
+function detailClose(targetId) {
+	$("#" + targetId).toggle();
+}
